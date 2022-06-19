982b7bf4be14b56ab131b9b148ef659f4fdf48d8
MunavvarSinan <munavvarsinan8@gmail.com>
2022-06-19 20:51:52 +0530

    added client and project functionality

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,6 +9,8 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLList,
+  GraphQLNonNull,
+  GraphQLEnumType,
 } = require('graphql');
 
 //Client
@@ -82,6 +84,123 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+//Mutations
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    // Add client
+    addClient: {
+      type: ClientType,
+      /*Arguments or args are the fiels that we need to add to create a user */
+      args: {
+        name: {
+          type: GraphQLNonNull(GraphQLString),
+        },
+        email: {
+          type: GraphQLNonNull(GraphQLString),
+        },
+        phone: {
+          type: GraphQLNonNull(GraphQLString),
+        },
+        /*GraphQLNonNull :- to avoid submitting with null value or without entering a name*/
+      },
+      resolve(parent, args) {
+        /*We are creating a new client and passing the value from the arguments */
+        const client = new Client({
+          name: args.name,
+          email: args.email,
+          phone: args.phone,
+        });
+        return client.save();
+      },
+    },
+    // Delete client
+    deleteClient: {
+      type: ClientType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        return Client.findByIdAndDelete(args.id);
+      },
+    },
+    // Add Project
+    addProject: {
+      type: projectType,
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        description: { type: GraphQLNonNull(GraphQLString) },
+        status: {
+          type: new GraphQLEnumType({
+            name: 'ProjectStatus',
+            values: {
+              new: { value: 'Not started' },
+              progress: { value: 'In progress' },
+              completed: { value: 'Completed' },
+            },
+          }),
+          defaultValue: 'Not started',
+        },
+        clientId: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        const project = new Project({
+          name: args.name,
+          description: args.description,
+          status: args.status,
+          clientId: args.clientId,
+        });
+        return project.save();
+      },
+    },
+    // Delete project
+    deleteProject: {
+      type: projectType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        return Project.findByIdAndRemove(args.id);
+      },
+    },
+    // Update a project
+    updateProject: {
+      type: projectType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+        /* We are not using GraphQLNonNull because it is not mandatory to update the name or description 
+        even though if name or description is added we can update this one */
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: {
+          type: new GraphQLEnumType({
+            name: 'ProjectStatusUpdate',
+            values: {
+              new: { value: 'Not started' },
+              progress: { value: 'In progress' },
+              completed: { value: 'Completed' },
+            },
+          }),
+        },
+      },
+      resolve(parent, args) {
+        return Project.findByIdAndUpdate(
+          args.id,
+          {
+            $set: {
+              name: args.name,
+              description: args.description,
+              status: args.status,
+            },
+          },
+          { new: true } // new means if the project is not there it is gonna create that
+        );
+      },
+    },
+  },
+});
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation,
 });
