511181399d4dad7be4fe02a0320e5aad3276c9e5
MunavvarSinan <munavvarsinan8@gmail.com>
2022-06-23 23:20:36 +0530

    finished

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Home from './pages/Home';
+import Notfound from './pages/Notfound';
+import Projects from './pages/Project';
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -36,6 +38,8 @@ function App() {
           <div className='container'>
             <Routes>
               <Route path='/' element={<Home />} />
+              <Route path='/projects/:id' element={<Projects />} />
+              <Route path='*' element={<Notfound />} />
             </Routes>
           </div>
         </Router>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import Clients from './components/Clients';
-import AddClientModel from './components/AddClientModel';
-import Projects from './components/Projects';
+import Clients from '../components/Clients';
+import AddClientModel from '../components/AddClientModel';
+import AddProjectModel from '../components/AddProjectModel';
+import Projects from '../components/Projects';
 
 const Home = () => {
   return (
     <>
-      <AddClientModel />
+      <div className='d-flex gap-3 mb-4'>
+        <AddClientModel />
+        <AddProjectModel />
+      </div>
       <Projects />
+      <hr />
       <Clients />
     </>
   );
